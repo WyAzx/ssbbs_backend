a5f3d15c4a718d11696aec6fb671af3f1a14072d
wangyun <wy1581052323@126.com>
2018-12-13 10:10:41 +0800

    ADD: load env

--- ssbbs_backend/__init__.py
+++ ssbbs_backend/__init__.py
@@ -1,6 +1,14 @@
+import json
+import os
+
 import pymysql
 import logging
 
 LOG = logging.getLogger(__name__)
 
 pymysql.install_as_MySQLdb()
+
+with open("environment.json") as env_file:
+    env_data = json.load(env_file)
+    for key, val in env_data.items():
+        os.environ[key] = str(val)
